184faa445bbb1f34047b7c69da1e7ce5208e766d
Syeda_Esha <161529169+syeda-eshaa-7@users.noreply.github.com>
2025-01-22 03:52:42 +0000

    Update env.ts

--- src/sanity/env.ts
+++ src/sanity/env.ts
@@ -1,5 +1,5 @@
 export const apiVersion =
-  process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2025-01-14'
+  process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2024-12-24'
 
 export const dataset = assertValue(
   process.env.NEXT_PUBLIC_SANITY_DATASET,
